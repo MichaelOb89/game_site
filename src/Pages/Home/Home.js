d175c7d780ece6981fa7cc7093bb5dab30947225
moris96 <moriskhoudari@gmail.com>
2023-03-30 16:23:28 -0400

    styling for home page--added animated box per game

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,8 @@
 import { logOut } from "../../utilities/users-service"
 import { Link } from "react-router-dom"
 
+import styles from '../Home/Home.module.scss'
+
 export default function Home({setUser}){
     function handleLogOut(){
         logOut()
@@ -9,9 +11,24 @@ export default function Home({setUser}){
 
     return(
         <>
-            <h1>Home Page</h1>
-            <button onClick={handleLogOut}>Log out</button>
-            <Link to={`/tictactoe`}>TicTacToe</Link>
+            <h1 className={styles.homeText}>Home Page</h1>
+            <button className={styles.logoutBtn} onClick={handleLogOut}>Log out</button>
+
+            <div className={styles.gamesBox}>
+                <div className={styles.gameList}>
+                    <h2 className={styles.gameTxt1}>Game:</h2>
+                    <div className={styles.ticTacToe}><Link to={`/tictactoe`}>TicTacToe</Link></div>
+                </div>
+            </div>
+
+
+            {/* <div className={styles.gamesList}>
+                <div><Link to={`/tictactoe`}>TicTacToe</Link></div>
+                <div><Link to={`/tictactoe`}>TicTacToe</Link></div>
+                <div><Link to={`/tictactoe`}>TicTacToe</Link></div>
+
+            </div> */}
+            
         </>
     )
 }
