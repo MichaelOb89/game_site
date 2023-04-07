eb36afee8f11426139a50f01615052f3fa1a2c38
moris96 <moriskhoudari@gmail.com>
2023-04-07 14:00:56 -0400

    updated styles on home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -14,27 +14,38 @@ export default function Home({setUser}){
             <h1 className={styles.homeText}>Home Page</h1>
             <button className={styles.logoutBtn} onClick={handleLogOut}>Log out</button>
 
+            {/* tictactoe */}
             <div className={styles.gamesBox}>
                 <div className={styles.gameList}>
-                    <h2 className={styles.gameTxt1}>Game:</h2>
+                    <h2 className={styles.gameTxt1}>Tic Tac Toe:</h2>
                     <div className={styles.ticTacToe}><Link to={`/tictactoe`} target="_blank">TicTacToe</Link></div>
                 </div>
             </div>
 
+            {/* rockpaperscissorslizardspock */}
             <div className={styles.gamesBox}>
                 <div className={styles.gameList}>
-                    <h2 className={styles.gameTxt1}>Game:</h2>
+                    <h2 className={styles.gameTxt1}>Rock Paper Scissors Lizard Spock:</h2>
                     <div className={styles.ticTacToe}><Link to={`/rockpaperscissorslizardspock`} target="_blank">RockPaperScissorsLizardSpock</Link></div>
                 </div>
             </div>
 
+            {/* rockpaperscissors */}
             <div className={styles.gamesBox}>
                 <div className={styles.gameList}>
-                    <h2 className={styles.gameTxt1}>Game:</h2>
+                    <h2 className={styles.gameTxt1}>Rock Paper Scissors:</h2>
                     <div className={styles.ticTacToe}><Link to={`/rockpaperscissors`} target="_blank">OG RockPaperScissors</Link></div>
                 </div>
             </div>
 
+            {/* pokemon */}
+            <div className={styles.gamesBox}>
+                <div className={styles.gameList}>
+                    <h2 className={styles.gameTxt1}>Pokemon:</h2>
+                    <h3 className={styles.h3Txt1}>Made by: <a href="https://www.linkedin.com/in/moris-khoudari-abady/" target="_blank">Moris Khoudari</a></h3>
+                    <div className={styles.ticTacToe}><a href="https://poke-clone.netlify.app/" target="_blank">Pokemon</a></div>
+                </div>
+            </div>
             
         </>
     )
