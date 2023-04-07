1d12ec3ef6e49e65b990f37cf08bb7b7d1f99b64
MichaelOb89 <michaelobrien.t@hotmail.com>
2023-04-07 14:26:27 -0400

    Backend functions for RPSLS

---
+++ webSocketFunctions/Rock-Paper-Scissors/RPSManager.js
@@ -0,0 +1,35 @@
+const games = []
+
+exports.games = games
+
+exports.getGames() = () => {
+    games.map((game)=>{
+        return{
+            ...game,
+            numberOfPlayers: game.players.length
+        }
+    })
+}
+
+exports.createGame = (player, lobbyName) => {
+    const game = {
+        name: lobbyName,
+        player1: player,
+        player2: null,
+        p1Wins: 0,
+        p2Wins: 0,
+        p1CurrentPlay: null,
+        p2CurrentPlay: null
+    }
+    games.push(game)
+}
+
+exports.joinGame =(player, gameName) => {
+    const foundGame = games.find(game=>game.name==gameName)
+    foundGame.player2 = player
+}
+
+exports.play = (updatedGame) => {
+    const foundGame = games.find(game=>game.name==updatedGame.name)
+    //Game logic and response here!
+}
