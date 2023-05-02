46486bff67ce47e68d68f59489eba04f6120f6b7
MichaelOb89 <michaelobrien.t@hotmail.com>
2023-05-01 22:46:46 -0400

    Fix player creating a game and disconnecting before another player joins

--- webSocketFunctions/Rock-Paper-Scissors/disconnect.js
+++ webSocketFunctions/Rock-Paper-Scissors/disconnect.js
@@ -2,9 +2,13 @@ const {removePlayer, removeGame, findGame} = require('./RPSManager')
 const sendGames = require('./sendGames')
 
 module.exports = ({io, socket}) => () => {
-    if(findGame(socket)){
+    const foundGame = findGame(socket)
+    if(foundGame && foundGame.players.length==2){
         const newSocket = removePlayer(socket).players[0].id
         io.to(newSocket).emit('opponentLeft', 'opponent disconnected')
+        removeGame(socket)
+        sendGames(io)
+    }else if(foundGame && foundGame.players.length==1){
         removeGame(socket)
         sendGames(io)
     }
